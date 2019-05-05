c2c9b653c5748951d5b4b474e916c617476f4617
DoorSupporter <jiwon3346@gmail.com>
2019-05-05 23:18:26 +0900

    5CHF * 2 = 10CHF

---
+++ src/main/java/Franc.java
@@ -0,0 +1,17 @@
+public class Franc {
+
+    private int amount;
+
+    Franc(int amount){
+        this.amount = amount;
+    }
+    Franc times(int multiplier){
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object){
+        Franc franc = (Franc) object;
+        return franc.amount == this.amount;
+    }
+}
