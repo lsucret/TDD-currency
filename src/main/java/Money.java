6c9485c8dc867ad897b102cfacdd69ee0c17f81d
Hyowon Park <willow_weed@naver.com>
2023-09-29 19:26:35 +0900

    ch12. add plus method

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -33,5 +33,8 @@ public class Money {
         return money.amount == this.amount && currency().equals(money.currency());
     }
 
+    public Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
 
 }
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -31,4 +31,12 @@ public class TestClass {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
 }
