f93f3cf0bf5cbf21d09a21cedefa8a1438bfb32d
Hyowon Park <willow_weed@naver.com>
2023-10-12 00:35:16 +0900

    ch16. implementation of stub method : Sum.plus(), Expression.times()

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -15,8 +15,8 @@ public class Money implements Expression {
         return new Money(amount, "CHF");
     }
 
-    Expression times(int multiplier){
-        return new Money(amount * multiplier,currency);
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
 
     String currency(){
